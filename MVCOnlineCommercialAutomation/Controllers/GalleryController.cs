eb61d77739c64d9bfa3dab0d11d0eb7945b217f6
serifetoy <serifeetoy@gmail.com>
2023-11-23 23:14:39 +0300

    Gallery controller  and thumber folder added.

---
+++ MVCOnlineCommercialAutomation/Controllers/GalleryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCOnlineCommercialAutomation.Models.Classes;
+
+namespace MVCOnlineCommercialAutomation.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Gallery
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var products = context.Products.ToList();
+            return View(products);
+        }
+    }
+}
